6043fdc0ede1e777d9e7971a520f85af3df038f4
Omraje Sarode <omsarode31@gmail.com>
2025-09-06 21:29:29 +0530

    Add files via upload

---
+++ tree.cpp
@@ -0,0 +1,137 @@
+#include <iostream>
+using namespace std;
+
+// Node structure
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Create a new node
+Node* createNode(int value) {
+    Node* newNode = new Node();
+    newNode->data = value;
+    newNode->left = newNode->right = nullptr;
+    return newNode;
+}
+
+// Insert a node into BST
+Node* insert(Node* root, int value) {
+    if (root == nullptr) {
+        cout << "Inserted " << value << endl;
+        return createNode(value);
+    }
+    if (value < root->data) {
+        root->left = insert(root->left, value);
+    } else if (value > root->data) {
+        root->right = insert(root->right, value);
+    }
+    return root;
+}
+
+// Search in BST
+bool search(Node* root, int key) {
+    if (root == nullptr) return false;
+    if (root->data == key) return true;
+    else if (key < root->data) return search(root->left, key);
+    else return search(root->right, key);
+}
+
+// Find minimum node (used for deletion)
+Node* findMin(Node* root) {
+    while (root && root->left != nullptr)
+        root = root->left;
+    return root;
+}
+
+// Delete a node from BST
+Node* deleteNode(Node* root, int key) {
+    if (root == nullptr) return root;
+
+    if (key < root->data) {
+        root->left = deleteNode(root->left, key);
+    } else if (key > root->data) {
+        root->right = deleteNode(root->right, key);
+    } else {
+        // Node found
+        if (root->left == nullptr) {
+            Node* temp = root->right;
+            delete root;
+            return temp;
+        } else if (root->right == nullptr) {
+            Node* temp = root->left;
+            delete root;
+            return temp;
+        }
+        // Node with two children
+        Node* temp = findMin(root->right);
+        root->data = temp->data;
+        root->right = deleteNode(root->right, temp->data);
+    }
+    return root;
+}
+
+// Traversals
+void inorder(Node* root) {
+    if (root != nullptr) {
+        inorder(root->left);
+        cout << root->data << " ";
+        inorder(root->right);
+    }
+}
+
+void preorder(Node* root) {
+    if (root != nullptr) {
+        cout << root->data << " ";
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+void postorder(Node* root) {
+    if (root != nullptr) {
+        postorder(root->left);
+        postorder(root->right);
+        cout << root->data << " ";
+    }
+}
+
+int main() {
+    Node* root = nullptr;
+
+    // Insert nodes
+    root = insert(root, 50);
+    root = insert(root, 30);
+    root = insert(root, 70);
+    root = insert(root, 20);
+    root = insert(root, 40);
+    root = insert(root, 60);
+    root = insert(root, 80);
+
+    cout << "\nInorder Traversal: ";
+    inorder(root);
+    cout << "\nPreorder Traversal: ";
+    preorder(root);
+    cout << "\nPostorder Traversal: ";
+    postorder(root);
+    cout << endl;
+
+    // Search
+    int key = 40;
+    cout << "\nSearching for " << key << ": ";
+    if (search(root, key))
+        cout << "Found!" << endl;
+    else
+        cout << "Not Found!" << endl;
+
+    // Delete a node
+    cout << "\nDeleting 50...\n";
+    root = deleteNode(root, 50);
+
+    cout << "Inorder Traversal after Deletion: ";
+    inorder(root);
+    cout << endl;
+
+    return 0;
+}
